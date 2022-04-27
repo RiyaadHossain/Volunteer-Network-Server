c111bd9747d39c689e550a19ac76db410a43c7f0
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-27 15:42:55 +0600

    Get api added

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+require("dotenv").config();
 const app = express()
 const port = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://riyad:${process.env.PASS}@volunteernetwork.cpddi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  await client.connect()
+  const volunteerCollection = client.db('VolunteerDB').collection('Volunteer')
+
+  // 1. GET API
+  app.get('/volunteer', async (req, res) => {
+    const query = req.query
+    const cursor = volunteerCollection.find(query)
+    const result = await cursor.toArray()
+    res.send(result)
+  })
+
+  try {
+    
+  }
+  finally{}
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
